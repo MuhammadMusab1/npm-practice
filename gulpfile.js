0153089b701cbf4aed82341dd23c6a45b4ec5f82
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-06 19:51:06 -0600

    add gulp-clean to delete dist easily

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series} = require('gulp'); // require says 'look for gulp in the node_module folder'
-
+const concat = require('gulp-concat')
+const clean = require('gulp-clean');
 function pagesTask() {
   return src('src/pages/*.html').pipe(dest('dist/pages'));
 }
@@ -12,5 +13,10 @@ function stylesTask() {
   return src('src/styles/*.css').pipe(dest('dist/styles'));
 }
 
+function deleteTask() {
+  return src('dist', {read: false})
+  .pipe(clean());
+}
 
 exports.default = series(pagesTask, scriptTask, stylesTask);
+exports.d = deleteTask;
