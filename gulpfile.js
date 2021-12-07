4a2027fadc70df89801c2c87f950b9942bf82cc5
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-06 18:01:20 -0600

    create and add to distribution folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
+const { src, dest, series} = require('gulp'); // require says 'look for gulp in the node_module folder'
+
+function pagesTask() {
+  return src('src/pages/*.html').pipe(dest('dist/pages'));
+}
+
+function scriptTask() {
+  return src('src/scripts/*.js').pipe(dest('dist/scripts'));
+}
+
+function stylesTask() {
+  return src('src/styles/*.css').pipe(dest('dist/styles'));
+}
+
+
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
 }
 
-exports.default = defaultTask
+exports.default = series(pagesTask, scriptTask, stylesTask);
